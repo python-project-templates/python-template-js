2f9438bd33cb68f2145d64db1703863821766d05
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-04-09 23:17:40 -0400

    Update for new JS toolchain

--- js/build.mjs
+++ js/build.mjs
@@ -1,92 +1,42 @@
-import { NodeModulesExternal } from "@finos/perspective-esbuild-plugin/external.js";
-import { build } from "@finos/perspective-esbuild-plugin/build.js";
-import { transform } from "lightningcss";
+import { bundle } from "./tools/bundle.mjs";
+import { bundle_css } from "./tools/css.mjs";
+import { node_modules_external } from "./tools/externals.mjs";
 import { getarg } from "./tools/getarg.mjs";
+
+import { transform } from "lightningcss";
 import fs from "fs";
 import cpy from "cpy";
 
-const DEBUG = getarg("--debug");
-
-const COMMON_DEFINE = {
-  global: "window",
-  "process.env.DEBUG": `${DEBUG}`,
-};
-
-const BUILD = [
+const BUNDLES = [
   {
-    define: COMMON_DEFINE,
     entryPoints: ["src/ts/index.ts"],
-    plugins: [NodeModulesExternal()],
-    format: "esm",
-    loader: {
-      ".css": "text",
-      ".html": "text",
-    },
+    plugins: [node_modules_external()],
     outfile: "dist/esm/index.js",
   },
   {
-    define: COMMON_DEFINE,
     entryPoints: ["src/ts/index.ts"],
-    plugins: [],
-    format: "esm",
-    loader: {
-      ".css": "text",
-      ".html": "text",
-    },
     outfile: "dist/cdn/index.js",
   },
 ];
 
-async function compile_css() {
-  const process_path = (path) => {
-    const outpath = path.replace("src/css", "dist/css");
-    fs.mkdirSync(outpath, { recursive: true });
+async function build() {
+  // Bundle css
+  await bundle_css();
 
-    fs.readdirSync(path, { withFileTypes: true }).forEach((entry) => {
-      const input = `${path}/${entry.name}`;
-      const output = `${outpath}/${entry.name}`;
-
-      if (entry.isDirectory()) {
-        process_path(input);
-      } else if (entry.isFile() && entry.name.endsWith(".css")) {
-        const source = fs.readFileSync(input);
-        const { code } = transform({
-          filename: entry.name,
-          code: source,
-          minify: !DEBUG,
-          sourceMap: false,
-        });
-        fs.writeFileSync(output, code);
-      }
-    });
-  };
-
-  process_path("src/css");
-}
-
-async function copy_html() {
+  // Copy HTML
   fs.mkdirSync("dist/html", { recursive: true });
   cpy("src/html/*", "dist/html");
-  // also copy to top level
   cpy("src/html/*", "dist/");
-}
 
-async function copy_img() {
+  // Copy images
   fs.mkdirSync("dist/img", { recursive: true });
   cpy("src/img/*", "dist/img");
-}
 
-async function copy_to_python() {
+  await Promise.all(BUNDLES.map(bundle)).catch(() => process.exit(1));
+
+  // Copy from dist to python
   fs.mkdirSync("../python_template_js/extension", { recursive: true });
   cpy("dist/**/*", "../python_template_js/extension");
 }
 
-async function build_all() {
-  await compile_css();
-  await copy_html();
-  await copy_img();
-  await Promise.all(BUILD.map(build)).catch(() => process.exit(1));
-  await copy_to_python();
-}
-
-build_all();
+build();
